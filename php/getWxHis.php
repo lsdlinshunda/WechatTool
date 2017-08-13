1c2a63519247de100e2de3fc7ebbb425bfebafad
lsdlinshunda <923076444@qq.com>
2017-08-13 20:40:43 +0800

    小调整

--- php/getWxHis.php
+++ php/getWxHis.php
@@ -15,11 +15,10 @@ $url = str_replace("\r", "", $url);
 ob_start();
 fpassthru($f);
 fclose($f);
-file_put_contents($urlFile, ob_get_clean() );
+file_put_contents($urlFile, ob_get_clean());
 
 //将下一个将要跳转的$url变成js脚本，由anyproxy注入到微信页面中。
 echo $url;
 if ($url != "")
 	echo "<script>setTimeout(function(){window.location.href='".$url."';},1000);</script>";
 ?>
-
